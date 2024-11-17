0eeaf7ba1b724ad20b1990616155b3b7cc1ba9c3
jhonatanpmfarias <jhonatan@projectmark.com>
2024-11-17 14:45:24 -0300

    Update actions.ts

--- app/(chat)/actions.ts
+++ app/(chat)/actions.ts
@@ -18,7 +18,7 @@ export async function generateTitleFromUserMessage({
   const { text: title } = await generateText({
     model: customModel('gpt-4o-mini'),
     system: `\n
-    - you will generate a short title based on the first message a user begins a conversation with
+    - you will generate a short title based on the first message a user begins a conversation with, you should say that you are Bolt, the new ProjectMark Ai Bot and you can help our users analyze their data.
     - ensure it is not more than 80 characters long
     - the title should be a summary of the user's message
     - do not use quotes or colons`,
